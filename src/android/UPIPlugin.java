a76db453e415ae87e0c485a7c82fea386c59ae92
the-deadshot <thakurajay2537@gmail.com>
2023-03-18 13:32:28 +0530

    add build import

--- src/android/UPIPlugin.java
+++ src/android/UPIPlugin.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
